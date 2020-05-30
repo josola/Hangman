897c95151ad64f4348d5fd1c2ca3abd2b50c3a17
josola <jordan.sola@gmail.com>
2020-05-30 11:24:14 -0500

    Failed attempts data collection from user.

--- src/hangman.cpp
+++ src/hangman.cpp
@@ -9,9 +9,15 @@
 
 using std::cout;
 using std::endl;
+using std::cin;
 
 int main()
 {
     cout << "Starting a new game of hangman..." << endl;
+
+    int failed_attempts = 0;
+    cout << "How many failed attempts do you need? [1-25] ";
+    cin >> failed_attempts;
+    
     return 0;
 }
