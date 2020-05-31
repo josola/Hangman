849d168e541ec525ee46827a638227675a672133
josola <jordan.sola@gmail.com>
2020-05-31 10:27:05 -0500

    Random number generator initial commit.

--- src/random_word.cpp
+++ src/random_word.cpp
@@ -1,11 +1,15 @@
 #include "random_word.h"
+#include <random>
 
 int GenerateRandomNumber(int range)
 {
-    
+    srand(static_cast<unsigned int>(time(NULL)));
+    int output(rand() % range + 1);
+    return output;
 }
 
 string WordChooser(vector<string> word_container)
 {
-
+    string output;
+    return output;
 }
