e53ac107faa98d59a6656ade986effcef429f74b
josola <jordan.sola@gmail.com>
2020-05-31 09:27:42 -0500

    Initial cour file commit.

---
+++ src/read_file.cpp
@@ -0,0 +1,14 @@
+/*
+ * Takes file input and reads contents to a container to be processed.
+ * Written by Jordan Sola 2020
+ * This program uses the MIT license.
+ */
+
+#include <fstream>
+
+using std::fstream;
+
+int main()
+{
+    return 0;
+}
