35831e7b7dc5467a6a704975a34e4103fb00407c
josola <jordan.sola@gmail.com>
2020-06-02 07:58:04 -0500

    Input check program.

--- src/check_utility.cpp
+++ src/check_utility.cpp
@@ -1,6 +1,16 @@
 #include "check_utility.h"
+#include <iostream>
+
+using std::cin;
 
 bool CheckIntInput(const int input)
 {
-
+    if (!cin)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
 }
